d27e86199c79e17c0501f54ca911253ff33d7c87
Kevin Bruckert <agrabren@yahoo.com>
2012-08-01 15:00:29 -0500

    Rename pieces

--- include/linux/msm_rotator.h
+++ include/linux/msm_rotator.h
@@ -4,7 +4,7 @@
 #include <linux/types.h>
 #include <linux/msm_mdp.h>
 
-#define MSM_ROTATOR_IOCTL_MAGIC 'R'
+#define MSM_ROTATOR_IOCTL_MAGIC 'Q'
 
 #define MSM_ROTATOR_IOCTL_START   \
 		_IOWR(MSM_ROTATOR_IOCTL_MAGIC, 1, struct msm_rotator_img_info)
